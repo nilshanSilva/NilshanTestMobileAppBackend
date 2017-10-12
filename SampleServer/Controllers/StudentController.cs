2a34a99146b71844bdc2202f17936281b8670469
nilshansilva <nilshandevinda1@gmail.com>
2017-10-12 13:18:40 +0530

    First Commit Message (Comment)

---
+++ SampleServer/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SampleServer.Models;
+
+namespace SampleServer.Controllers
+{
+    public class StudentController : ApiController
+    {
+        // GET: api/Student
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Student/5
+        public StudentModel Get(int id)
+        {
+            return new StudentModel { Name = "Nilshan", Age = 21, School = "Esoft" };
+        }
+
+        // POST: api/Student
+        public void Post([FromBody]StudentModel value)
+        {
+            StudentModel Model = value;
+        }
+
+        // PUT: api/Student/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Student/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SampleServer/Models/StudentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleServer.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string School { get; set; }
+    }
+}
