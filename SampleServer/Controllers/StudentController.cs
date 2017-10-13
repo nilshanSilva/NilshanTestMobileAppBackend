944e64356cc412cc0cdad42114b8f9b54a71fecc
nilshansilva <nilshandevinda1@gmail.com>
2017-10-13 22:26:09 +0530

    connectionstring update

--- SampleServer/Controllers/StudentController.cs
+++ SampleServer/Controllers/StudentController.cs
@@ -10,17 +10,17 @@ namespace SampleServer.Controllers
 {
     public class StudentController : ApiController
     {
+        DatabaseContext context = new DatabaseContext();
         // GET: api/Student
-        public IEnumerable<string> Get()
+        public IEnumerable<StudentModel> Get()
         {
-            
-            return new string[] { "Nilshan", "Steve", "Yeshan" };
+            return context.StudentModel.AsEnumerable();
+           // return new string[] { "Nilshan", "Steve", "Yeshan" };
         }
 
         // GET: api/Student/5
         public StudentModel Get(int id)
         {
-            DatabaseContext context = new DatabaseContext();
             return context.StudentModel.Find(id);
            // return new StudentModel { Name = "Nilshan", Age = 21, School = "Esoft" };
         }
@@ -28,19 +28,23 @@ namespace SampleServer.Controllers
         // POST: api/Student
         public void Post([FromBody]StudentModel value)
         {
-            DatabaseContext context = new DatabaseContext();
             context.StudentModel.Add(value);
             context.SaveChanges();
         }
 
         // PUT: api/Student/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]StudentModel value)
         {
+            StudentModel model = context.StudentModel.Find(id);
+            context.Entry(model).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
         }
 
         // DELETE: api/Student/5
         public void Delete(int id)
         {
-        }//
+            context.Entry(context.StudentModel.Find(id)).State = System.Data.Entity.EntityState.Deleted;
+            context.SaveChanges();
+        }
     }
 }
